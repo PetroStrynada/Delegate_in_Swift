8c4e99ab0eefd6b41d20030a799983846b8a5801
Petro Strynada <petro.strynada@gmail.com>
2023-08-18 16:00:40 +0300

    Added functionality
    class Engineer through protocol EngineerDelegate interacts with
    any subscribed class on that protocol (ProjectManager, Client, CEO...)
    in example with class ProjectManager

--- DelegatePractise.playground/Contents.swift
+++ DelegatePractise.playground/Contents.swift
@@ -5,6 +5,8 @@ var greeting = "Hello, playground"
 //AnyObject now we can make delegate as weak property
 protocol EngineerDelegate: AnyObject {
     func tasksHasEnded()
+    func didFinishResearch(data: String)
+    func didFinishAnalyzeRequirements(result: [String])
 }
 
 class Engineer {
@@ -18,29 +20,65 @@ class Engineer {
         }
     }
 
-    func taskDone() {
-        tasks -= 1
+    func taskDone(_ taskDone: Int = 1) {
+        tasks -= taskDone
+    }
+
+    func startResearch(_ data: String =  "") {
+        //google
+        //create doment
+        //review doc
+        delegate?.didFinishResearch(data: "document")
+    }
+
+    func analyze(requirements: [String]) {
+        //read
+        //questions
+        //document
+        delegate?.didFinishAnalyzeRequirements(result: ["question 1", "question 2"])
     }
 }
 
 class projectManager {
     var engineer: Engineer?
 
-    func addTaskToEngineer() {
-        engineer?.tasks += 1
+    func addTaskToEngineer(_ numberOfTasks: Int = 1) {
+        engineer?.tasks += numberOfTasks
     }
 }
 
 extension projectManager:EngineerDelegate {
     func tasksHasEnded() {
-        print("Give me new tasks")
+        print("Client: Give me new tasks")
+    }
+
+    func didFinishResearch(data: String) {
+        addTaskToEngineer(2)
+    }
+
+    func didFinishAnalyzeRequirements(result: [String]) {
+        print("Client: Clarify requirements")
     }
 }
 
-var engener = Engineer()
+var engineer = Engineer()
 var manager = projectManager()
-engener.delegate = manager
-manager.engineer = engener
+engineer.delegate = manager
+manager.engineer = engineer
 manager.addTaskToEngineer()
 
-engener.tasks
+engineer.tasks
+engineer.taskDone()
+
+engineer.analyze(requirements: [])
+
+engineer.tasks
+engineer.startResearch()
+engineer.tasks
+
+engineer.taskDone(2)
+
+engineer.tasks
+// 1. protocol
+// 2. obj1: property weak var delegate: ProtocolDelegate?
+// 3. obj2: :ProtocolDelegate
